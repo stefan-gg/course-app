9a7575d0536ea8d0bf895d75123ce3956dfbbdd9
stefan-gg <stefan.gogic01@gmail.com>
2023-05-03 04:50:35 +0200

    Throttle classes added. Project is finished for now.

--- backend/courseapp/settings.py
+++ backend/courseapp/settings.py
@@ -144,6 +144,14 @@ REST_FRAMEWORK = {
     # ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 SIMPLE_JWT = {
